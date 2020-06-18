9235f45d6aea29c765bd9f96fc1be177e1337bec
Theo Gaillard <theogllrd@gmail.com>
2020-06-18 17:12:40 +0200

    team page done

--- src/containers/Team.js
+++ src/containers/Team.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const Data = [
+	{
+		id: 1,
+		url: '../images/team/ayman.jpg',
+		name: 'OUSSAMA AYMAN',
+		surnom: 'Le geek',
+		seriePref: 'How I met your mother',
+		pourcentageNote: '30',
+	},
+	{
+		id: 2,
+		url: '../images/team/paul.jpg',
+		name: 'KLAK PAUL',
+		surnom: "L'animal",
+		seriePref: 'Joséphine ange gardien',
+		pourcentageNote: '30',
+	},
+	{
+		id: 3,
+		url: '../images/team/leandre.jpg',
+		name: 'CARPENTIER LEANDRE',
+		surnom: 'Le ponctuel',
+		seriePref: 'Des racines et des ailes',
+		pourcentageNote: '30',
+	},
+	{
+		id: 4,
+		url: '../images/team/theo.jpg',
+		name: 'GAILLARD THEO',
+		surnom: 'Le chiant',
+		seriePref: 'Drucker le dimanche',
+		pourcentageNote: '30',
+	},
+];
+
 class Team extends Component {
 	render() {
-		return <p>Team</p>;
+		return (
+			<div>
+				{Data.map((value, index) => {
+					return (
+						<div className="card" key={value.id}>
+							<div className="content">
+								<img
+									src={value.url}
+									alt={value.name}
+									style={{ width: '100%' }}
+								/>
+								<div className="infos">
+									<h2 className="name">{value.name}</h2>
+									<p className="title">{value.surnom}</p>
+									<p>Mon coup de coeur: {value.seriePref}</p>
+									<p>{value.pourcentageNote}% de la note finale</p>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		);
 	}
 }
 
